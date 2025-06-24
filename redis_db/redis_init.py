47e6ab1f6816237dc2d7f101c57033d96106f214
Prince Poudel <84308512+Prince-7154@users.noreply.github.com>
2025-06-24 11:52:42 +0545

    Update redis_init.py

--- redis_db/redis_init.py
+++ redis_db/redis_init.py
@@ -6,9 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 psw = os.getenv("Redis_password")
+url = os.getenv("Redis_url")
 
 redis_client = redis.Redis(
-    host='redis-15358.c283.us-east-1-4.ec2.redns.redis-cloud.com',
+    host= url,
     port=15358,
     decode_responses=True,
     username="default",
